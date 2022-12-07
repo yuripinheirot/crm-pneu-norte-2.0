7b11969234f8bc1b899db83b148224ff806ddfae
Yuri Pinheiro <yurii1408@hotmail.com>
2022-12-07 18:58:07 -0300

    feat: create question model

---
+++ project/project/domain/model/Question.model.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project.domain.model
+{
+    internal class Question
+    {
+        public int id { get; set; }
+        public string description { get; set; }
+        public string module { get; set; }
+        public bool active { get; set; } = true;
+    }
+}
