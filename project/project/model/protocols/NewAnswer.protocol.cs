7aa23a9c1fab8731ef12da70f5fd63a525a8b1fb
Yuri Pinheiro <yurii1408@hotmail.com>
2022-12-06 23:31:41 -0300

    feat: add new protocols to AddCrm

---
+++ project/project/model/protocols/NewAnswer.protocol.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace project.model.protocols
+{
+    internal class NewAnswer
+    {
+        public int id { get; set; }
+        public int idQuestion { get; set; }
+        public string idSale { get; set; }
+        public string idClient { get; set; }
+        public string status { get; set; }
+        public string answer { get; set; }
+        public string observation { get; set; }
+        public string resolution { get; set; }
+        public DateTime updatedAt { get; set; }
+        public string idCompany { get; set; }
+    }
+}
---
+++ project/project/model/usecases/IAddCrm.usecase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using project.model.protocols;
+
+namespace project.model.usecases
+{
+	interface IAddCrm
+	{
+		void AddCrm(NewAnswer[] answers);		
+	}
+}
