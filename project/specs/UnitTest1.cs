185313cc015e63fc651f94b6895952b7010e1624
Yuri Pinheiro <yurii1408@hotmail.com>
2022-12-24 10:07:31 -0300

    chore: add specs solution

---
+++ project/specs/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using project.business.usecases.answers;
+
+namespace specs
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var test = new AnswersBusiness();
+        }
+    }
+}
