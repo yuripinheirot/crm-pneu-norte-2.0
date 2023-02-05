31ce4b7102fadb95e0e01ed809c72bfd7e0a6477
Yuri Pinheiro <yurii1408@hotmail.com>
2023-02-05 09:11:47 -0300

    test: GetQuestionsShouldReturnCorrectValues

--- project/specs/unit/data/Questions.data.spec.cs
+++ project/specs/unit/data/Questions.data.spec.cs
@@ -26,5 +26,45 @@ namespace specs.unit.data
             });
         }
 
+        [TestMethod]
+        [Description("getQuestions should return correct values")]
+        public void GetQuestionsShouldReturnCorrectValues()
+        {
+            List<QuestionModel> questions = new List<QuestionModel>()
+            {
+                new QuestionModel()
+                {
+                    id = "idd",
+                    active = true,
+                    answers = new List<string>(){"yess", "noo"},
+                    badAnswers = new List<string>(){"noo"},
+                    description = "descriptionn",
+                    posSale = "posSales"
+                },
+                new QuestionModel()
+                {
+                    id = "id",
+                    active = false,
+                    answers = new List<string>(){"yes", "no"},
+                    badAnswers = new List<string>(){"no"},
+                    description = "description",
+                    posSale = "posSale"
+                }
+            };
+
+            QuestionsDataFactoryMock.questionRepositoryMock
+                .Setup(x => x.getQuestions())
+                .Returns(questions);
+
+            var questionsReturned = QuestionsDataFactoryMock.questionsData.getQuestions();
+
+            Assert.AreEqual(2, questionsReturned.Count);
+            Assert.AreEqual(questions[0].id, questionsReturned[0].id);
+            Assert.AreEqual(questions[0].active, questionsReturned[0].active);
+            CollectionAssert.AreEqual(questions[0].answers, questionsReturned[0].answers);
+            CollectionAssert.AreEqual(questions[0].badAnswers, questionsReturned[0].badAnswers);
+            Assert.AreEqual(questions[0].description, questionsReturned[0].description);
+            Assert.AreEqual(questions[0].posSale, questionsReturned[0].posSale);
+        }
     }
 }
