d6b229a11ca3377f1ca00caef0c47ed16090bf9f
Yuri Pinheiro <yurii1408@hotmail.com>
2023-02-05 10:32:45 -0300

    test: ShouldGetSaleReturnCorrectValues

---
+++ project/specs/feature/postgres/Sales.firebird.repository.test.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using project.domain.model;
+using project.infra.db.postgres.repository;
+using project.presentation.protocols;
+using specs.suport.factories.feature;
+using specs.suport.helpers;
+using System;
+using System.Collections.Generic;
+
+namespace specs.feature.postgres
+{
+    [TestClass]
+    public class SalesFirebirdRepositoryTest
+    {
+        [TestMethod]
+        [Description("Should getSale return correct values")]
+        public void ShouldGetSaleReturnCorrectValues()
+        {
+
+            var sale = SalesRepositoryFactoryMock.salesRepository.getSale("02", "0000200");
+
+            Assert.IsNotNull(sale);
+            Assert.AreEqual("0000200", sale.id);
+            Assert.AreEqual("02", sale.idCompany);
+            Assert.AreEqual((decimal)399.01, sale.liquidValue);
+            Assert.AreEqual(new DateTime(2022, 03, 12), sale.dateSale);
+            Assert.AreEqual("20240-JUNIOR BERLANDA E OUTROS", sale.client);
+            Assert.AreEqual("95111441115", sale.clientCpfCnpj);
+            Assert.AreEqual("sale ", sale.posSale);
+            Assert.AreEqual("CRISTIANO", sale.seller);
+        }
+    }
+}
---
+++ project/specs/suport/factories/feature/Sales.repository.factory.mock.cs
@@ -0,0 +1,23 @@
+﻿using Moq;
+using project.business.usecases.answers;
+using project.data.usecases.answers;
+using project.data.usecases.questions;
+using project.domain.interfaces.Struct;
+using project.infra.db.postgres.config;
+using project.infra.db.postgres.repository;
+using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using project.infra.db.firebird.config;
+using project.infra.db.firebird.repository;
+
+namespace specs.suport.factories.feature
+{
+    public class SalesRepositoryFactoryMock
+    {
+        public static SalesFirebirdRepository salesRepository = new SalesFirebirdRepository();
+    }
+}
