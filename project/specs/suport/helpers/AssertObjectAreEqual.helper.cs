9edc8172aa90510d6165d0592e1a78f8084807c9
Yuri Pinheiro <yurii1408@hotmail.com>
2023-02-05 20:04:08 -0300

    test: create a function to verify each properties in object

---
+++ project/specs/suport/helpers/AssertObjectAreEqual.helper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace specs.suport.helpers
+{
+    public class AssertObjectAreEqualHelper
+    {
+        public static void verify(object expected, object actual)
+        {
+            PropertyInfo[] properties = expected.GetType().GetProperties();
+            foreach (var property in properties)
+            {
+                object expectedValue = property.GetValue(expected);
+                object actualValue = property.GetValue(actual);
+                Assert.AreEqual(expectedValue, actualValue);
+            }
+        }
+    }
+}
