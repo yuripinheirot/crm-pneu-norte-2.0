eb50a8e87ae3ab51cf6b56f74b61de4117fd6d9a
Yuri Pinheiro <yurii1408@hotmail.com>
2023-02-05 11:09:39 -0300

    refactor: test all sales

--- project/specs/feature/firebird/Sales.firebird.repository.test.cs
+++ project/specs/feature/firebird/Sales.firebird.repository.test.cs
@@ -45,13 +45,16 @@ namespace specs.feature.firebird
 
             Assert.AreEqual(3, sales.Count);
             Assert.AreEqual("0000200", sales[0].id);
-            Assert.AreEqual("02", sales[0].idCompany);
             Assert.AreEqual((decimal)399.01, sales[0].liquidValue);
             Assert.AreEqual(new DateTime(2022, 03, 12), sales[0].dateSale);
-            Assert.AreEqual("20240-JUNIOR BERLANDA E OUTROS", sales[0].client);
             Assert.AreEqual("95111441115", sales[0].clientCpfCnpj);
-            Assert.AreEqual("sale ", sales[0].posSale);
             Assert.AreEqual("CRISTIANO", sales[0].seller);
+            sales.ForEach(s =>
+            {
+                Assert.AreEqual("02", s.idCompany);
+                Assert.AreEqual("sale ", s.posSale);
+                Assert.AreEqual("20240-JUNIOR BERLANDA E OUTROS", s.client);
+            });
         }
     }
 }
