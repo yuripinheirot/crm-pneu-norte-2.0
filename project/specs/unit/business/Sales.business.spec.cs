f7188552badfebf9403dfa36ea8c5b47bff125dc
Yuri Pinheiro <yurii1408@hotmail.com>
2023-02-05 20:48:27 -0300

    refactor: clean code

--- project/specs/unit/business/Sales.business.spec.cs
+++ project/specs/unit/business/Sales.business.spec.cs
@@ -1,11 +1,11 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using Moq;
-using System.Collections.Generic;
 using project.domain.model;
-using specs.suport.factories.unit.business;
 using project.presentation.protocols;
+using specs.suport.factories.unit.business;
 using specs.suport.helpers;
+using System;
+using System.Collections.Generic;
 
 namespace specs
 {
