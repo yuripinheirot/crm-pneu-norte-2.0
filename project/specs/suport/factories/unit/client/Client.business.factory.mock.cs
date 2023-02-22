2831a1eb8a21914572cc0abe833c10a168692181
Yuri Pinheiro <yurii1408@hotmail.com>
2023-02-22 17:58:04 -0300

    test: fix suport mock

--- project/specs/suport/factories/unit/client/Client.business.factory.mock.cs
+++ project/specs/suport/factories/unit/client/Client.business.factory.mock.cs
@@ -2,13 +2,15 @@
 using project.business.usecases.client;
 using project.data.usecases.client;
 using project.domain.interfaces.Struct;
+using project.infra.db.firebird.config;
 using project.infra.db.firebird.repository;
+using project.infra.db.postgres.config;
 
 namespace specs.suport.factories.unit.business
 {
     public class ClientBusinessFactoryMock
     {
-        public static Mock<ClientFirebirdRepository> clientRepository = new Mock<ClientFirebirdRepository>();
+        public static Mock<ClientFirebirdRepository> clientRepository = new Mock<ClientFirebirdRepository>(new FbDbContext(), new PgDbContext());
         public static Mock<ClientData<IClientRepository>> clientDataMock = new Mock<ClientData<IClientRepository>>(clientRepository.Object);
 
         public static ClientBusiness<IClientData> clientBusiness = new ClientBusiness<IClientData>(clientDataMock.Object);
