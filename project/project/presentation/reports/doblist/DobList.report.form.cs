4c2544c430eb779b7e22259fc729fb57adf55cde
Yuri Pinheiro <yuripinheirot@gmail.com>
2025-04-21 21:33:44 -0300

    refactor: extract data preparation and report configuration into separate methods in DobList report form for improved readability and maintainability

--- project/project/presentation/reports/doblist/DobList.report.form.cs
+++ project/project/presentation/reports/doblist/DobList.report.form.cs
@@ -12,6 +12,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using project.data.usecases.client;
+using project.presentation.protocols;
 
 namespace project.presentation.reports.doblist
 {
@@ -24,6 +25,47 @@ namespace project.presentation.reports.doblist
             InitializeComponent();
         }
 
+        DataTable prepareDt(List<DobListReport> clients)
+        {
+
+            DataTable dt = new DataTable();
+            dt.TableName = "MainReport";
+
+            dt.Columns.Add("clientId");
+            dt.Columns.Add("clientName");
+            dt.Columns.Add("observation");
+            dt.Columns.Add("done");
+            dt.Columns.Add("dob");
+
+            foreach (var client in clients)
+            {
+                dt.Rows.Add(client.clientId, client.clientName, client.observation, client.done, client.dob);
+            }
+
+            return dt;
+        }
+
+        private void configureReport(Report report, DataTable dt, string dti, string dtf)
+        {
+            report.Load(Path.Combine(Environment.CurrentDirectory + @"\presentation\reports\doblist\dobList.report.frx"));
+
+            report.SetParameterValue("initialDate", DateTime.Parse(dti).ToString("dd/MM/yyyy"));
+            report.SetParameterValue("finalDate", DateTime.Parse(dtf).ToString("dd/MM/yyyy"));
+
+            report.RegisterData(dt, dt.TableName);
+
+            report.GetDataSource(dt.TableName).Enabled = true;
+
+            DataBand dataBand2 = (DataBand)report.FindObject("Data2");
+            DataBand dataBand3 = (DataBand)report.FindObject("Data3");
+
+            dataBand2.DataSource = report.GetDataSource(dt.TableName);
+            dataBand3.DataSource = report.GetDataSource(dt.TableName);
+
+            // Open report in designer
+            // report.Design(true);
+        }
+
         private void btnExport_Click(object sender, EventArgs e)
         {
             try
@@ -33,35 +75,11 @@ namespace project.presentation.reports.doblist
 
                 var clients = getClientsByDob.execute(dti, dtf);
 
-                DataTable dt = new DataTable();
-                dt.TableName = "MainReport";
-
-                dt.Columns.Add("clientId");
-                dt.Columns.Add("clientName");
-                dt.Columns.Add("observation");
-                dt.Columns.Add("done");
-                dt.Columns.Add("dob");
-
-                foreach (var client in clients)
-                {
-                    dt.Rows.Add(client.clientId, client.clientName, client.observation, client.done, client.dob);
-                }
 
                 Report report = new Report();
-                report.Load(Path.Combine(Environment.CurrentDirectory + @"\presentation\reports\doblist\dobList.report.frx"));
-                report.SetParameterValue("initialDate", DateTime.Parse(dti).ToString("dd/MM/yyyy"));
-                report.SetParameterValue("finalDate", DateTime.Parse(dtf).ToString("dd/MM/yyyy"));
-                report.RegisterData(dt, dt.TableName);
 
-                // Enable data source for designer
-                report.GetDataSource(dt.TableName).Enabled = true;
-                DataBand dataBand2 = (DataBand)report.FindObject("Data2");
-                DataBand dataBand3 = (DataBand)report.FindObject("Data3");
-                dataBand2.DataSource = report.GetDataSource(dt.TableName);
-                dataBand3.DataSource = report.GetDataSource(dt.TableName);
-
-                // Open report in designer
-                // report.Design(true);
+                var dt = prepareDt(clients);
+                configureReport(report, dt, dti, dtf);
 
                 report.Show();
             }
